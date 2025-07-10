40a26f847db296e7bc88199755a6dc1ff917484b
Aditya Kalra <aditya.kalra.cs@gmail.com>
2025-07-10 23:14:41 +0530

    Added Functionality to Button

--- src/components/atoms/Button.jsx
+++ src/components/atoms/Button.jsx
@@ -1,9 +1,9 @@
 
 
-export const Button = ({children}) =>{
+export const Button = ({children,onClick}) =>{
     return (
         <>
-            <button className="px-5 py-3 bg-green-500 hover:bg-green-800 m-1 transition cursor-pointer rounded-xl shadow-md">{children}</button>
+            <button className="px-5 py-3 bg-green-500 hover:bg-green-800 m-1 transition cursor-pointer rounded-xl shadow-md" onClick={onClick}>{children}</button>
         </>
     )
 }
--- src/components/organism/ContentDis.jsx
+++ src/components/organism/ContentDis.jsx
@@ -1,12 +1,18 @@
 import { Button } from "../atoms/Button"
 import { Content } from "../molecules/Content"
 
+
 export const ContentDis = () =>{
+
+    function showAlert (){
+    alert("Hi, Button's Onclick Given from Organism Level >> Molecule Level >> Atoms Level >> Button Component");
+}
+
     return(
         <>
-        <div className="text-center shadow-md flex justify-center items-center flex-col p-40 cursor-pointer transition hover:ring-1 ring-green-500 rounded-xl hover:shadow-2xl hover:scale-110 duration-750">
+        <div className="text-center shadow-md flex justify-center items-center flex-col p-40 cursor-pointer transition hover:ring-2 ring-green-500 rounded-xl hover:shadow-2xl hover:scale-110 duration-750">
             <Content></Content>
-            <Button>Click Me</Button>
+            <Button onClick={showAlert}>Click Me</Button>
         </div>
         </>
     )
